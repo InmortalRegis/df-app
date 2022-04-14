ed5707b7d363eca6b2ae5fc4aa13291b0c22f1a3
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2022-04-14 14:14:35 -0500

    feat: product sizes components

--- src/components/Product/ProductSizes.js
+++ src/components/Product/ProductSizes.js
@@ -3,63 +3,57 @@ import "./ProductSizes.css";
 import TwitterIcon from "../Icons/Twitter";
 import FacebookIcon from "../Icons/Facebook";
 import WhatsappIcon from "../Icons/Whatsapp";
+
+const SocialNetworks = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <TwitterIcon
+        style={{
+          marginRight: "10px",
+        }}
+      />
+      <FacebookIcon
+        style={{
+          marginRight: "10px",
+        }}
+      />
+      <WhatsappIcon />
+    </div>
+  );
+};
+
+const SizesRow = ({ sizes }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        overflowX: "auto",
+        width: "90vw",
+        marginBottom: "1.5rem",
+      }}
+    >
+      {sizes.map((s, i) => (
+        <div className="product-sizes__size" key={i}>
+          {s.size}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 const ProductSizes = ({ sizes }) => {
   return (
     <div className="product-sizes">
       <p className="product-sizes__title">TALLE</p>
+      <SizesRow sizes={sizes} />
 
-      <div
-        style={{
-          display: "flex",
-          overflowX: "auto",
-          width: "90vw",
-          marginBottom: "1.5rem",
-        }}
-      >
-        {sizes.map((s, i) => (
-          <div className="product-sizes__size" key={i}>
-            {s.size}
-          </div>
-        ))}
-      </div>
-
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <TwitterIcon
-          style={{
-            marginRight: "10px",
-          }}
-        />
-        <FacebookIcon
-          style={{
-            marginRight: "10px",
-          }}
-        />
-        <WhatsappIcon />
-      </div>
-      {/* <p className="product-sizes__title">TALLE</p>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          overflowX: "auto",
-          overflowY: "hidden",
-          whiteSpace: "nowrap",
-        }}
-      >
-        <div>
-          {sizes.map((s, i) => (
-            <div className="product-sizes__size" key={i}>
-              {s.size}
-            </div>
-          ))}
-        </div>
-      </div> */}
+      <SocialNetworks />
     </div>
   );
 };
